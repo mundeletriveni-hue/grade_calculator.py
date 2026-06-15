a5a8e4350b82da7b46ba3cb28e8a0388b290c39a
mundeletriveni-hue <mundeletriveni@gmail.com>
2026-06-15 12:29:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+import streamlit as st
 
+st.title("Grade Calculator")
+
+name = st.text_input("Enter Student Name")
+
+marks = st.number_input(
+    "Enter Marks (0-100)",
+    min_value=0,
+    max_value=100
+)
+
+if st.button("Calculate Grade"):
+
+    if marks >= 90:
+        grade = "A"
+    elif marks >= 80:
+        grade = "B"
+    elif marks >= 70:
+        grade = "C"
+    elif marks >= 60:
+        grade = "D"
+    else:
+        grade = "F"
+
+    st.write("Student Name:", name)
+    st.write("Marks:", marks)
+    st.success(f"Grade: {grade}")
